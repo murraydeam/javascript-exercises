c6a728f8ede449381dc0f03b781216b0b15fe8c3
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-08-19 14:01:45 -0400

    Error tests 1-5 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let givenString = "";
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  while (num > 0) {
+    givenString += string;
+    num--;
+  }
+  return givenString;
 };
 
 // Do not edit below this line
